8ba299f8746807213f3a06a081bdbe92cc9df7ad
Benjamin McLean <mclean.ben@gmail.com>
2016-11-28 18:19:57 -0600

    Started building test

--- tests/gdx-tests/src/com/badlogic/gdx/tests/OrthogonalTiledMapIteratorTest.java
+++ tests/gdx-tests/src/com/badlogic/gdx/tests/OrthogonalTiledMapIteratorTest.java
@@ -1,18 +1,127 @@
 package com.badlogic.gdx.tests;
 
+import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.graphics.*;
+import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+import com.badlogic.gdx.graphics.g2d.TextureAtlas;
+import com.badlogic.gdx.graphics.g2d.TextureRegion;
+import com.badlogic.gdx.graphics.glutils.FrameBuffer;
+import com.badlogic.gdx.maps.MapLayers;
+import com.badlogic.gdx.maps.tiled.TiledMap;
+import com.badlogic.gdx.maps.tiled.TiledMapRenderer;
+import com.badlogic.gdx.maps.tiled.TiledMapTileLayer;
+import com.badlogic.gdx.maps.tiled.iterators.OrthogonalTiledMapIterator;
+import com.badlogic.gdx.maps.tiled.renderers.OrthogonalTiledMapRenderer;
+import com.badlogic.gdx.maps.tiled.tiles.StaticTiledMapTile;
 import com.badlogic.gdx.tests.utils.GdxTest;
+import com.badlogic.gdx.utils.viewport.FitViewport;
+import com.badlogic.gdx.utils.viewport.Viewport;
 
 /**
  * Created by Benjamin on 11/28/2016.
  */
 public class OrthogonalTiledMapIteratorTest extends GdxTest {
+    public static final int VIRTUAL_WIDTH = 64;
+    public static final int VIRTUAL_HEIGHT = 64;
+    public static final int TILE_WIDTH = 16;
+    public static final int TILE_HEIGHT = 16;
+    public static final int SIZE_X = 64;
+    public static final int SIZE_Y = 64;
+    private Color worldBackgroundColor = Color.BLACK;
+    private Color screenBackgroundColor = Color.BLACK;
+    public Texture one;
+    public TextureAtlas.AtlasRegion water;
+    public TextureAtlas.AtlasRegion grass;
+    private SpriteBatch batch = new SpriteBatch();
+    private TiledMap map = new TiledMap();
+    private TiledMapRenderer tiledMapRenderer;
+    private FrameBuffer frameBuffer = new FrameBuffer(Pixmap.Format.RGBA8888, VIRTUAL_WIDTH, VIRTUAL_HEIGHT, true, true);
+    private Viewport worldView = new FitViewport(VIRTUAL_WIDTH, VIRTUAL_HEIGHT);
+    private Viewport screenView = new FitViewport(VIRTUAL_WIDTH, VIRTUAL_HEIGHT);
+    private Texture screenTexture;
+    private TextureRegion screenRegion = new TextureRegion();
+    private OrthogonalTiledMapIterator visibleIterator;
+    public int playerX = SIZE_X / 2, playerY = SIZE_Y / 2;
+
     @Override
     public void create () {
+        Pixmap pixmap1 = new Pixmap(1, 1, Pixmap.Format.RGBA8888);
+        pixmap1.drawPixel(0, 0, -1);
+        one = new Texture(pixmap1);
+        one.setFilter(Texture.TextureFilter.Nearest, Texture.TextureFilter.Nearest);
 
+        pixmap1.setColor(Color.BLUE);
+        pixmap1.fill();
+        Texture wallTesture = new Texture(pixmap1);
+        wallTesture.setFilter(Texture.TextureFilter.Nearest, Texture.TextureFilter.Nearest);
+        water = new TextureAtlas.AtlasRegion(wallTesture, 0, 0, 16, 16);
+
+        pixmap1.setColor(Color.GREEN);
+        pixmap1.fill();
+        Texture floorTesture = new Texture(pixmap1);
+        floorTesture.setFilter(Texture.TextureFilter.Nearest, Texture.TextureFilter.Nearest);
+        grass = new TextureAtlas.AtlasRegion(floorTesture, 0, 0, 16, 16);
+
+        pixmap1.dispose();
+
+        MapLayers layers = map.getLayers();
+        TiledMapTileLayer layer = new TiledMapTileLayer(SIZE_X, SIZE_Y, TILE_WIDTH, TILE_HEIGHT);
+        for (int x = 0; x < SIZE_X; x++) {
+            for (int y = 0; y < SIZE_Y; y++) {
+                StaticTiledMapTile tile = null;
+                if (isLand(x, y))
+                    tile = new StaticTiledMapTile(grass);
+                else
+                    tile = new StaticTiledMapTile(water);
+                layer.setCell(x, y, new TiledMapTileLayer.Cell().setTile(tile));
+            }
+        }
+        layers.add(layer);
+        tiledMapRenderer = new OrthogonalTiledMapRenderer(map);
     }
 
     @Override
     public void render () {
+        frameBuffer.begin();
+        Gdx.gl.glClearColor(worldBackgroundColor.r, worldBackgroundColor.g, worldBackgroundColor.b, 1);
+        Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
+        worldView.apply();
+        worldView.getCamera().position.set(playerX * TILE_HEIGHT + (TILE_HEIGHT / 2), playerY * TILE_WIDTH + (TILE_WIDTH / 2), 0);
+        worldView.update(VIRTUAL_WIDTH, VIRTUAL_HEIGHT);
+        tiledMapRenderer.setView((OrthographicCamera) worldView.getCamera());
+        tiledMapRenderer.render();
+        batch.setProjectionMatrix(worldView.getCamera().combined);
+        batch.begin();
 
+        batch.end();
+        frameBuffer.end();
+
+        Gdx.gl.glClearColor(screenBackgroundColor.r, screenBackgroundColor.g, screenBackgroundColor.b, 1);
+        Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
+
+        screenView.apply();
+        batch.setProjectionMatrix(screenView.getCamera().combined);
+        batch.begin();
+        screenTexture = frameBuffer.getColorBufferTexture();
+        screenTexture.setFilter(Texture.TextureFilter.Nearest, Texture.TextureFilter.Nearest);
+        screenRegion.setRegion(screenTexture);
+        screenRegion.flip(false, true);
+        batch.draw(screenRegion, 0, 0);
+        batch.end();
+    }
+
+    public Boolean isLand (int x, int y) {
+        return x % 2 == 1 && y % 2 == 1;
+    }
+
+    public void drawRect(SpriteBatch batch, int x, int y, int width, int height) {
+        batch.draw(one, x + width - 1, y + 1, 1, height - 1);
+        batch.draw(one, x + 1, y, width - 1, 1);
+        batch.draw(one, x, y, 1, height - 1);
+        batch.draw(one, x, y + height - 1, width - 1, 1);
+    }
+
+    public void drawSquareOverTile(SpriteBatch batch, int x, int y) {
+        batch.draw(one, x * TILE_WIDTH, y * TILE_HEIGHT, TILE_WIDTH, TILE_HEIGHT);
     }
 }
